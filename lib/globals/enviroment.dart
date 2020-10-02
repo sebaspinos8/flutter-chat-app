e1b233e2b847e67c6074cb58c54ca69c17599c18
Sebas Pinos <csebaspinosm8@hotmail.com>
2020-10-01 21:07:04 -0500

    Final

--- lib/globals/enviroment.dart
+++ lib/globals/enviroment.dart
@@ -2,10 +2,10 @@ import 'dart:io';
 
 class Enviroment {
   static String apiUrl = Platform.isAndroid
-      ? 'https://flutter-chat-sebas.herokuapp.com/api/login/api'
-      : 'https://flutter-chat-sebas.herokuapp.com/api/login/api';
+      ? 'https://flutter-chat-sebas.herokuapp.com/api'
+      : 'https://flutter-chat-sebas.herokuapp.com/api';
 
   static String socketUrl = Platform.isAndroid
-      ? 'https://flutter-chat-sebas.herokuapp.com/api/login/'
-      : 'https://flutter-chat-sebas.herokuapp.com/api/login/';
+      ? 'https://flutter-chat-sebas.herokuapp.com'
+      : 'https://flutter-chat-sebas.herokuapp.com';
 }
